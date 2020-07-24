ed149fd0e6f5688b41fdad71ac88a89f36b85b52
Ayelen Etchegoyen <aye.etche@gmail.com>
2020-07-24 02:48:42 -0300

    config

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+const port = process.env.PORT || 3000;
+app.listen(port);
